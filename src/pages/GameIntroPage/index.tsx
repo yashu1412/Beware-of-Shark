1916f322d19703bf7eef564a11a59b35d982ca79
yashu1412 <135001156+yashu1412@users.noreply.github.com>
2024-12-28 21:46:46 +0530

    Updated name of Folder

---
+++ src/pages/GameIntroPage/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import GameIntroView from '../../view/GameIntroView/index'
+
+const GameIntroPage = () => {
+  return (
+    <div>
+      <GameIntroView />
+    </div>
+  )
+}
+
+export default GameIntroPage
