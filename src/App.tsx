5236b15af0ab9d089b9cd25c643d82701a193cb0
yashu1412 <135001156+yashu1412@users.noreply.github.com>
2025-01-13 00:42:34 +0530

    added files and updated code

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import Router from './router'
-
+import './App.css'
 const App: React.FC = () => {
   return (
     <div>
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App'
-import reportWebVitals from './reportWebVitals'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux'; // Import Provider from react-redux
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import store from './Store'; // Import the Redux store
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}> {/* Wrap App with Provider */}
+      <App />
+    </Provider>
   </React.StrictMode>,
-)
+);
 
 // Start measuring performance
-reportWebVitals(console.log) // Logs performance metrics to the console
+reportWebVitals(console.log); // Logs performance metrics to the console
