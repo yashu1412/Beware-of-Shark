a517c3c19f149f21d03109082d9e7d7f80ad5f6a
yashu1412 <135001156+yashu1412@users.noreply.github.com>
2025-01-13 00:37:03 +0530

    Update index.tsx

--- src/containers/GameOverContainer/index.tsx
+++ src/containers/GameOverContainer/index.tsx
@@ -1,22 +1,38 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
-import GameOverView from '../../View/GameOverView'
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import GameOverView from "../../view/GameOverView";
+
 const GameOverContainer: React.FC = () => {
-  const [isScoreVisible, setIsScoreVisible] = useState(false)
+  const [isScoreVisible, setIsScoreVisible] = useState(false);
+
+
+  const finalScore = useSelector((state: any) => state.game.score);
+  const correctAnswers = useSelector((state: any) => state.game.correctAnswers);
+  const wrongAnswers = useSelector((state: any) => state.game.wrongAnswers);
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      setIsScoreVisible(true)
-    }, 5000)
+      setIsScoreVisible(true);
+    }, 5000);
 
-    return () => clearTimeout(timer)
-  }, [])
+    return () => clearTimeout(timer);
+  }, []);
+
+
+  useEffect(() => {
+    console.log("Game Over! Total Score:", finalScore);
+    console.log("Correct Answers:", correctAnswers);
+    console.log("Wrong Answers:", wrongAnswers);
+  }, [finalScore, correctAnswers, wrongAnswers]);
 
   return (
-    <>
-      <GameOverView isScoreVisible={isScoreVisible} />
-    </>
-  )
-}
+    <GameOverView
+      isScoreVisible={isScoreVisible}
+      score={finalScore}
+      correctAnswers={correctAnswers}
+      wrongAnswers={wrongAnswers}
+    />
+  );
+};
 
-export default GameOverContainer
+export default GameOverContainer;
